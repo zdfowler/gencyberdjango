18fc34eb2901a9598273e52882bec45d7acc0f0a
mlhale <mlhale@unomaha.edu>
2016-07-01 13:21:25 -0700

    cleanup

--- api/views.py
+++ api/views.py
@@ -17,100 +17,53 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework.permissions import *
 
 class Session(APIView):
-    permission_classes = (AllowAny,)
-    def form_response(self, isauthenticated, userid, username, error=""):
-        data = {
-            'isauthenticated': isauthenticated,
-            'userid': userid,
-            'username': username
-        }
-        if error:
-            data['message'] = error
+	permission_classes = (AllowAny,)
+	def form_response(self, isauthenticated, userid, username, error=""):
+		data = {
+			'isauthenticated': isauthenticated,
+			'userid': userid,
+			'username': username
+		}
+		if error:
+			data['message'] = error
 
-        return Response(data)
+		return Response(data)
 
-    def get(self, request, *args, **kwargs):
-        # Get the current user
-        if request.user.is_authenticated():
-            return self.form_response(True, request.user.id, request.user.username)
-        return self.form_response(False, None, None)
+	def get(self, request, *args, **kwargs):
+		# Get the current user
+		if request.user.is_authenticated():
+			return self.form_response(True, request.user.id, request.user.username)
+		return self.form_response(False, None, None)
 
-    def post(self, request, *args, **kwargs):
-        # Login
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return self.form_response(True, user.id, user.username)
-            return self.form_response(False, None, None, "Account is suspended")
-        return self.form_response(False, None, None, "Invalid username or password")
+	def post(self, request, *args, **kwargs):
+		# Login
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return self.form_response(True, user.id, user.username)
+			return self.form_response(False, None, None, "Account is suspended")
+		return self.form_response(False, None, None, "Invalid username or password")
 
-    def delete(self, request, *args, **kwargs):
-        # Logout
-        logout(request)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+	def delete(self, request, *args, **kwargs):
+		# Logout
+		logout(request)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
 def home(request):
-  """
-  Send requests to / to the ember.js clientside app  """
-  
-  return render_to_response('index.html',
-    {}, RequestContext(request))
+	"""
+	Send requests to / to the ember.js clientside app  """
 
-
-#more detailed, but more control class based view example
-class ForumpostList(APIView):
-    """
-    List all forum posts, or create a new post.
-    """
-    def get(self, request, format=None):
-        posts = Forumpost.objects.all() #you could limit this to only the posts for which the user has access
-        serializer = ForumpostSerializer(posts, many=True, context={'request': request})
-        return Response(serializer.data) #you can customize the response here
-
-    def post(self, request, format=None):
-        serializer = ForumpostSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) #you could customize the response here
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #you could customize the error message here
-
-
-class ForumpostDetail(APIView):
-    """
-    Retrieve, update or delete a single forum post.
-    """
-    def get_object(self, pk):
-        try:
-            return Forumpost.objects.get(pk=pk)
-        except Forumpost.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = ForumpostSerializer(post, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = ForumpostSerializer(post, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+	return render_to_response('index.html',
+		{}, RequestContext(request))
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+	"""
+	API endpoint that allows users to be viewed.
+	"""
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
