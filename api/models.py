e979931e4678a769775b1bdd417f9eeda1e8aefe
mlhale <mlhale@unomaha.edu>
2016-07-06 14:02:35 -0700

    added in admin page, fixed a few database structure issues for like model

--- api/models.py
+++ api/models.py
@@ -2,15 +2,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import *
+from django.contrib import admin
 
 # Create your models here.
 class Like(models.Model):
+	user =  models.ForeignKey(User)
 	title = models.CharField(max_length=100, blank=False)
 	objid = models.IntegerField(blank=False)
 	farm = models.CharField(max_length=100, blank=False)
 	secret = models.CharField(max_length=100, blank=False)
 	server = models.CharField(max_length=100, blank=False)
 
+	def __str__(self):
+		return self.title
+
+	class Admin(admin.ModelAdmin):
+		list_display = ('title', 'objid', 'user')
+	
 class Userprofile(models.Model):
 	user = models.ForeignKey(User)
 	likes = models.ManyToManyField(Like)
+
+	def __str__(self):
+		return self.user.username
+
+	class Admin(admin.ModelAdmin):
+		list_display = ('user',)
