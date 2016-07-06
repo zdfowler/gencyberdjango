09487887186012266e44e18430d46a1c4b9212e8
mlhale <mlhale@unomaha.edu>
2016-07-06 12:36:03 -0700

    gencyber tutorial release version

--- api/views.py
+++ api/views.py
@@ -17,51 +17,6 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework.permissions import *
 from rest_framework.decorators import *
 
-class Session(APIView):
-	permission_classes = (AllowAny,)
-	def form_response(self, isauthenticated, userid, username, error=""):
-		data = {
-			'isauthenticated': isauthenticated,
-			'userid': userid,
-			'username': username
-		}
-		if error:
-			data['message'] = error
-
-		return Response(data)
-
-	def get(self, request, *args, **kwargs):
-		# Get the current user
-		if request.user.is_authenticated():
-			return self.form_response(True, request.user.id, request.user.username)
-		return self.form_response(False, None, None)
-
-	def post(self, request, *args, **kwargs):
-		# Login
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				return self.form_response(True, user.id, user.username)
-			return self.form_response(False, None, None, "Account is suspended")
-		return self.form_response(False, None, None, "Invalid username or password")
-
-	def delete(self, request, *args, **kwargs):
-		# Logout
-		logout(request)
-		return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-def home(request):
-	"""
-	Send requests to / to the ember.js clientside app  """
-
-	return render_to_response('index.html',
-		{}, RequestContext(request))
-
 class UserViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed.
